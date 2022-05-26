5a580735dcc3de7c10da4923e9b377087eb95bd1
Pau Ruiz Martin <101583566+PauRuiz02@users.noreply.github.com>
2022-05-26 20:57:47 +0200

    Añadimos el ejercicio

---
+++ Practica2Codigo/src/practica2codigo/Biblioteca.java
@@ -0,0 +1,15 @@
+package practica2codigo;
+
+
+import java.util.List;
+
+public class Biblioteca {
+
+    private List<Llibre> llibres;
+
+    public Biblioteca(List<Llibre> llibres) {
+        this.llibres = llibres;
+    }
+    
+    
+}
